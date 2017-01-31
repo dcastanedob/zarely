d063bd57c11844bb94510e38d15edaa419441278
Armando Alonso <arm_chiva@hotmail.com>
2017-01-31 10:38:47 -0600

    Creando los catalogos

--- module/Application/src/Application/Catalogo/Form/TallajesForm.php
+++ module/Application/src/Application/Catalogo/Form/TallajesForm.php
@@ -30,10 +30,557 @@ class TallajesForm extends Form
             'name' => 'talla_70',
             'type' => 'Checkbox',
             'attributes' => array(
-                'required' => true,
+                'required' => false,
                 'class' => 'form-control infput-thick',
             ),
         ));
+
+        $this->add(array(
+            'name' => 'talla_75',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_80',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_85',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_90',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+         $this->add(array(
+            'name' => 'talla_95',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+          $this->add(array(
+            'name' => 'talla_100',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+
+           $this->add(array(
+            'name' => 'talla_105',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_110',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_115',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_120',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_125',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_130',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_135',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_140',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_145',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_150',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+
+        $this->add(array(
+            'name' => 'talla_155',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_160',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_165',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_170',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_175',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_180',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_185',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_190',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_195',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_200',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_205',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_210',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_215',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_220',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_225',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_230',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_235',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_240',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_245',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_250',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_255',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_260',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_265',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_270',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_275',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_280',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_285',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_290',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_295',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_300',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_305',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_310',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_315',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_320',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_325',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_330',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_335',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_340',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'talla_345',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
+        $this->add(array(
+            'name' => 'talla_350',
+            'type' => 'Checkbox',
+            'attributes' => array(
+                'required' => false,
+                'class' => 'form-control infput-thick',
+            ),
+        ));
+
+
 
         
     }
---
+++ public/js/catalogo/productos.js
@@ -0,0 +1,126 @@
+(function( $ ){
+    
+   
+   /*
+    * Handle input. Call public functions and initializers
+    */
+   
+    $.fn.empleados= function(data){
+        var _this = $(this);
+        var plugin = _this.data('empleados');
+        
+        /*Inicializado ?*/
+        if (!plugin) {
+            
+            plugin = new $.empleados(this, data);
+            
+            _this.data('empleados', plugin);
+            
+            return plugin;
+        /*Si ya fue inizializado regresamos el plugin*/    
+        }else{
+            return plugin;
+        }
+        
+    };
+    
+    /*
+    * Plugin Constructor
+    */
+   
+    $.empleados = function(container, options){
+        
+        var plugin = this;
+       
+       /* 
+        * Important Components
+        */ 
+        var $container = $(container);  
+        
+        var settings;
+
+        var defaults = {
+        
+       };
+        
+
+        plugin.init = function(){
+
+            settings = plugin.settings = $.extend({}, defaults, options);
+
+        }
+
+        plugin.list =function(){
+            var $table = $container.find('.dataTable').dataTable(
+                {
+                    language: {
+                      url: "/json/datatable_es.json",
+
+                    },
+                    serverSide:true,
+
+                    ajax:{
+                        method: 'POST',
+                        url:'/catalogo/empleados/serverside',
+                    },
+                    columns:[
+                        {"data":"idempleado","name":"idempleado","orderable":true},
+                        {"data":"empleado_nombre","name":"empleado_nombre","orderable":true},
+                        {"data":"empleado_telefono","name":"empleado_telefono","orderable":true},
+                        {"data":"empleado_email","name":"empleado_email","orderable":true},
+                        {"data":"empleado_estatus","name":"empleado_estatus","orderable":true},
+                        {"data":"options","name":"options","orderable":false, class:"td_options"},
+                    ],
+                    proccesing: true,
+
+                    createdRow: function(row,data,index){
+                        $(row).find('.delete_modal').on('click',function(){
+                            var id = $(row).attr('id');
+                            var tmpl = [
+                            '<div id="modalBounceInLeft" tabindex="-1" role="dialog" class="modal in">',
+                                  '<div class="modal-dialog">',
+                                   ' <div class="modal-content animated bounceInLeft">',
+                                    '  <div class="modal-header">',
+                                       ' <button type="button" class="close" data-dismiss="modal">',
+                                          '<span aria-hidden="true">×</span>',
+                                          '<span class="sr-only">Close</span>',
+                                        '</button>',
+                                      '</div>',
+                                      '<form action="/catalogo/empleados/eliminar/'+id+'" method="POST">',
+                                     ' <div class="modal-body">',
+                                        '<div class="text-center">',
+                                         ' <span class="text-primary icon icon-times-circle icon-5x"></span>',
+                                          '<h3 class="text-primary">Advertencia</h3>',
+                                          '<p>¿Estás seguro que deseas eliminar el registro seleccionado?</p>',
+                                         ' <div class="m-t-lg">',
+                                          '  <button class="btn btn-primary" type="submit">Eliminar</button>',
+                                           ' <button class="btn btn-default" data-dismiss="modal" type="button">Cancelar</button>',
+                                          '</div>',
+                                        '</div>',
+                                      '</div>',
+                                      '<div class="modal-footer"></div>',
+                                    '</div>',
+                                  '</div>',
+                                '</div>',
+                            ].join('');
+
+                            $(tmpl).modal();
+                        });
+                    }
+                }
+            );
+
+
+        }
+
+        /*
+        * Plugin initializing
+        */
+        
+        plugin.init();
+       
+    }
+    
+    
+    
+})( jQuery );
